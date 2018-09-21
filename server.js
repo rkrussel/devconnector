d1afcba1fd396ad148084027f025ffbb9e4fdee9
Richard Krussel <richardkrussel@Richards-MacBook-Pro.local>
2018-09-20 21:45:35 -0500

    add additional server configuration

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
+const passport = require('passport');
 const app = express();
+//Configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//configure passport
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+//Configure Database
 const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db)
     .then(() => console.log("MongoDB connected."))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello '));
+
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
